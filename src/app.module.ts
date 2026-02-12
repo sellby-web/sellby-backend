7a7b56d8ba3873d07144cb31ea3999e612559439
Rahul Gopinathan <rahulthettikulam@gmail.com>
2026-02-12 23:09:46 +0000

    Create controller, service, module for all the application modules like message, user, asset

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AdvertisementModule } from './advertisement/advertisement.module';
+import { AssetModule } from './asset/asset.module';
+import { MessageModule } from './message/message.module';
+import { WishlistModule } from './wishlist/wishlist.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    AdvertisementModule,
+    AssetModule,
+    MessageModule,
+    WishlistModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
